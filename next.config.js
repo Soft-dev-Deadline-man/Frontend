af850ff87da91290ee59b9164de94f14ac787a82
Skul1CrowN <boat.72545@gmail.com>
2023-10-01 00:15:47 +0700

    Add & Fix: Add env to workflows and Fix docker deploy.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },images: {
-        domains: ['cdn1.iconfinder.com','api.slingacademy.com'],
-      },
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["cdn1.iconfinder.com", "api.slingacademy.com"],
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

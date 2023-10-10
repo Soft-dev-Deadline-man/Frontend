1fee374a1a76d0492f7643ee7a81503b9f77bbbe
Pattharaphorn Ch <janchandecha@gmail.com>
2023-10-10 13:59:17 +0700

    Create Background Location all

--- src/app/nologin/layout.tsx
+++ src/app/nologin/layout.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { NavbarNoLogin }  from "@/components/NavbarNoLogin";
-
-export const metadata = {
-    title: 'Roadtrip in CHON',
-    description: 'The Tarvelling Website',
-}
 
 export default function NoLoginLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className="bg-state-800">
-                <NavbarNoLogin />
-                {children}
-
-            </body>
-        </html>
-    );
+  return <div>{children}</div>;
 }
--- src/app/nologin/page.tsx
+++ src/app/nologin/page.tsx
@@ -2,15 +2,54 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import bg from "/src/app/assets/bg.png";
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/navigation";
+import { signIn, signOut, getSession } from "next-auth/react";
 
-export default function NoLogin(){
+export default function NoLogin() {
+
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const status = await signIn("credentials", {
+      redirect: false,
+      email: data.get("email"),
+      password: data.get("password"),
+      callbackUrl: "/",
+    });
+  }
   return (
-    <div>
-     <h1>Hello</h1>
-     <Link href='/'> Link to Home page </Link>
-    </div>
+
+    <>
+      <Navbar />
+      <div>
+        <Image
+          alt="img"
+          src={bg}
+          width={1812}
+          height={426}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
+        {/* <img
+          src="https://s3-alpha-sig.figma.com/img/7f08/dd87/d50c3f4b536f8d68cede11522fe6e352?Expires=1697414400&Signature=SkAQ9A~~LcfPzcLinf0OorPW2Eed0YJqAOYvVtzZ8s9q~xF~Bq2zNPCxbjSog~FvoIXXaxP-OaNfoCH~gXYNR7UxJXwWBVhBrTc0Nv-kRUNgE5LKNuZVK0ZRqQhbL~5bqtclOVb0s4I8MW0fjfw4vHKszGAanX3oniAXN-c~0q41mxZQaDGfKyavsb-3dZl87LBxW9JwkbSK7c2E3tF7ai4yw0~VVAmLE0iEmUm7FqGw9GeFUw-1WO84GthnSPgQ1YRzqUb344fozz77ONd9jdvjP2dqJhh14EF1lcpLRioNPwCsaGqdABy5WFYfEF5e85H7OnTStkDkqY0XYepgKA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          sizes="100vw"
+          width={1600}
+          height={200}
+        /> */}
+      </div>
+      <div>
+
+      </div>
+
+    </>
+
+
+
+
   );
 }
-
-  
-

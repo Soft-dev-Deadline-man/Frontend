ffeec4050aaafcbe3dd375346bab6943a12240d5
Pattharaphorn Ch <janchandecha@gmail.com>
2023-10-16 05:11:28 +0700

    Update no-login page

--- src/app/nologin/page.tsx
+++ src/app/nologin/page.tsx
@@ -3,14 +3,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import bg from "/src/app/assets/bg.png";
-import pic1 from "/src/app/assets/pic1.png";
-import pic2 from "/src/app/assets/pic2.png";
-import pic3 from "/src/app/assets/pic3.png";
-import pic4 from "/src/app/assets/pic4.png";
-import pic5 from "/src/app/assets/pic5.png";
-import pic6 from "/src/app/assets/pic6.png";
 import Navbar from "@/components/Navbar";
-import LocationBox from "@/components/LocationBox";
+import Checkcetagory from "@/components/Checkcetagory";
+import Checktime from "@/components/Checktime";
+import Checkanother from "@/components/Checkanother";
+
+import LocationBoxnologin from "@/components/LocationBoxnologin";
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSession, signIn, signOut } from "next-auth/react";
@@ -21,273 +19,80 @@ import { FaCalendarDay } from "react-icons/fa";
 import { FaClock } from "react-icons/fa";
 import { IBlog } from "@/types/Blog";
 import axios from "axios";
+import ButtonGroup from "@/components/ButtonGroup";
+import Searchbar from "@/components/Searchbar";
+
 
 export default function NoLogin({ blogInfo }: { blogInfo: IBlog | null | any }) {
-
   const [firstImage, setFirstImage] = useState<any>(null)
   const { data: session, status } = useSession();
   const { push } = useRouter();
   const [blog, setBlog] = useState<IBlog | null | any>(null)
 
+  const handleSubmit = (searchTerm: any) => {
+    console.log(`Search term submitted: ${searchTerm}`);
+  };
+  const inputProps = {
+    onFocus: () => {
+      console.log('Input field focused');
+    },
+    onBlur: () => {
+      console.log('Input field blurred');
+    },
+  };
+
   useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_BACKEND as string}blogs`).then((res) => {
-      setBlog(res.data)
-    })
+    try {
+      axios.get(`${process.env.NEXT_PUBLIC_BACKEND as string}blogs`).then((res) => {
+        setBlog(res.data)
+      })
+    } catch (error) {
+      console.error(error);
+
+    }
   }, [])
   console.log(blog)
   return blog && (
     <>
       <Navbar />
-      {blog.map((blog : any,key : any)=>{
-        console.log(blog)
-        return(
-          <LocationBox blogInfo={blog} key={key}/>
-        )
-      })}
-      
-      <div>
-        <Image
-          alt="img"
-          src={bg}
-          width={1900}
-          height={326}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-      </div>
 
-      <div className="">
-        {/* nologin place 1 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 1"
-                  src={pic1}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5 mx-11">
-              <div className="block mt-1 text-lg font-medium text-black">
-                จุดชมวิวพัทยา
-              </div>
-              <div>
-                <a href="#" className="uppercase tracking-wide text-black">
-                  อำเภอบางละมุง ชลบุรี
-                </a>
-              </div>
+      <div className="mt-[80px]">
 
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-
-              <div className="flex items-center mt-1 gap-2">
-                <FaClock />
-                <p className="mt-1 text-slate-500">เปิดบริการ 24 ชั่วโมง</p>
-              </div>
-
-              <div></div>
-
-              <p className="mt-2 text-slate-500">ชายหาด และทะเล</p>
-
-
-            </div>
+        <div className="w-full">
+          <div className="mx-auto flex-col flex  mb-14 overflow-hidden max-w-8xl space-y-5
+        ">
+            {blog.map((blog: any, key: any) => {
+              console.log(blog)
+              return (
+                <LocationBoxnologin blogInfo={blog} key={key} />
+              )
+            })}
           </div>
         </div>
 
-        {/* nologin place 2 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 2"
-                  src={pic2}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5  mx-11">
-              <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                วัดเขาชีจรรย์
-              </div>
-              <a href="#" className="uppercase tracking-wide text-black">
-                นาจอมเทียน อำเภอสัตหีบ ชลบุรี
-              </a>
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-              <p className="mt-2 text-slate-500">08.00 - 18.00 น.</p>
-              <div></div>
-              <p className="mt-2 text-slate-500">
-                ประวัติศาสตร์ วัฒนธรรมและศาสนา
-              </p>
-            </div>
-          </div>
+        <div>
+          <Image
+            alt="img"
+            src={bg}
+            width={1900}
+            height={326}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
         </div>
 
-        {/* nologin place 3 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 3"
-                  src={pic3}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5 mx-11">
-              <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                สวนนงนุชพัทยา
-              </div>
-              <a href="#" className="uppercase tracking-wide text-black">
-                ตำบล นาจอมเทียน อำเภอสัตหีบ ชลบุรี
-              </a>
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-              <p className="mt-2 text-slate-500">08.00 - 18.00 น.</p>
-              <div></div>
-              <p className="mt-2 text-slate-500">
-                สวนสัตว์ สวนน้ำ และสวนสนุก
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* nologin place 4 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 4"
-                  src={pic4}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5 mx-11">
-              <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                พิพิธภัณฑ์สัตว์น้ำ
-              </div>
-              <a href="#" className="uppercase tracking-wide text-black">
-                ตำบล บางละมุง อำเภอศรีราชา ชลบุรี
-              </a>
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-              <p className="mt-2 text-slate-500">10.30 - 18.00 น.</p>
-
-              <p className="mt-2 text-slate-500">
-                สวนสัตว์ สวนน้ำ และสวนสนุก
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* nologin place 5 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 5"
-                  src={pic5}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5 mx-11">
-              <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                หาดเทียน เกาะล้าน{" "}
-              </div>
-              <a href="#" className="uppercase tracking-wide text-black">
-                ตำบล บางพระ อำเภอศรีราชา ชลบุรี
-              </a>
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-              <p className="mt-2 text-slate-500">เปิดบริการ 24 ชั่วโมง</p>
-
-              <p className="mt-2 text-slate-500">ชายหาด และทะเล</p>
-            </div>
-          </div>
-        </div>
-
-
-        {/* nologin place 6 */}
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">
-              <div className="h-48 w-full object-cover md:h-full md:w-48">
-                <Image
-                  alt="img pic 6"
-                  src={pic6}
-                  width={448}
-                  height={1400}
-                  style={{
-                    maxWidth: "120%",
-                    height: "110%",
-                  }}
-                />
-              </div>
-            </div>
-            <div className="p-5 mx-11">
-              <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                หาดทรายขาว
-              </div>
-              <a href="#" className="uppercase tracking-wide text-black">
-                ตำบล บางละมุง อำเภอเมือง ชลบุรี
-              </a>
-              <div className="flex items-center mt-1 gap-2">
-                <FaCalendarDay />
-                <p className="mt-1 text-slate-500">เปิดบริการทุกวัน</p>
-              </div>
-              <p className="mt-2 text-slate-500">09.30 - 17.00 น.</p>
-
-              <p className="mt-2 text-slate-500">ชายหาด และทะเล</p>
-            </div>
-          </div>
+        <div className="max-w-md p-10 divide-y-2 bg-[#FAFBFC] rounded-2xl shadow-md overflow-hidden md:max-w-2xl space-y-10">
+          <Searchbar onSubmit={handleSubmit} inputProps={inputProps} />
+          < Checkcetagory />
+          < Checktime />
+          < ButtonGroup buttons={["0+", "1+", "2+", "3+", "4+"]} />
+          < Checkanother />
         </div>
 
       </div>
     </>
   );
 }
+

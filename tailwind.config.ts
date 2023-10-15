a822ed2673ff4b432910d0e551bc8d19c76fed6d
Pattharaphorn Ch <janchandecha@gmail.com>
2023-10-16 04:50:02 +0700

    Update Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js'
   ],
   theme: {
     extend: {
@@ -18,14 +20,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         map: "url('/map.png')",
         person: "url('/image71.png')",
+        
       },
       fontFamily: {
         preahvihear: ["var(--font-preahvihear)"],
         karla: ["var(--font-roboto-karla)"],
       },
-      animation:{
-        
-      }
+      animation: {},
     },
   },
   darkMode: "class",

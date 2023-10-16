49726cd7ec4b5dbf2f3170d57ff3460cb700a487
Pattharaphorn Ch <janchandecha@gmail.com>
2023-10-16 16:28:51 +0700

    Update Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,26 @@
 import type { Config } from "tailwindcss";
-const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/button.js",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js'
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        base: "url('/base.png')",
-        sea: "url('/Rectangle3.png')",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        map: "url('/map.png')",
-        person: "url('/image71.png')",
-        
       },
       fontFamily: {
         preahvihear: ["var(--font-preahvihear)"],
         karla: ["var(--font-roboto-karla)"],
+        karnit: ["var(--font-karnit)"],
       },
       animation: {},
     },
   },
   plugins: [require("daisyui")],
-}
-export default config
+};
+export default config;
